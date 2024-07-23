c3aa6b188db17184bf6c1770def57096db61f4f3
Huseyin-Celebi <hcelebi057@gmail.com>
2024-07-24 00:44:49 +0300

    firs

---
+++ src/main/java/dev/huseyin/library/LiteraryLand/LiteraryLandApplication.java
@@ -0,0 +1,13 @@
+package dev.huseyin.library.LiteraryLand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraryLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraryLandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/huseyin/library/LiteraryLand/LiteraryLandApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.huseyin.library.LiteraryLand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraryLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
